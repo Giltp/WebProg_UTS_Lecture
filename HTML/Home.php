c97589334cadfba5c24da50d164d62551dec4ee7
AnthonioMasbroo <129573492+AnthonioMasbroo@users.noreply.github.com>
2023-10-21 22:05:34 +0700

    Update and rename Home.html to Home.php

---
+++ HTML/Home.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF330Restaurant</title>
+    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section id="Home">
+    <?php
+        include 'Header.php';
+    ?>
+        <div class="main">
+            <div class="men_text">
+                <h1>5<span>Star</span><br>Restaurant</h1>
+            </div>
+        </div>
+    </section>
+
+    <!--Menu-->
+
+    <div class="menu" id="Menu">
+        <h1>Our<span>Menu</span></h1>
+        <div class="menu_box">
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Dessert/Brownies.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Brownies</h2>
+                    <p>
+                        Chocolate brownies dessert
+                    </p>
+                    <h3>$8.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Dessert/Croissants.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Croissant</h2>
+                    <p>
+                        One of the most popular french food
+                    </p>
+                    <h3>$15.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Dessert/Ice_Cream.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Ice Cream</h2>
+                    <p>
+                        Mixed flavour ice cream
+                    </p>
+                    <h3>$5.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Dessert/Pie.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Pie</h2>
+                    <p>
+                        Strawberry pie fresh from the oven
+                    </p>
+                    <h3>$7.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Lauk/Biryani_Rice.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Biryani</h2>
+                    <p>
+                        One of the traditional food from India
+                    </p>
+                    <h3>$20.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Lauk/Fried_Rice.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Fried Rice</h2>
+                    <p>
+                        Indonesian fried rice made by Indonesian chef
+                    </p>
+                    <h3>$20.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Lauk/Ramen.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Ramen</h2>
+                    <p>
+                        one of the most popular food from Japan
+                    </p>
+                    <h3>$20.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Minuman/Coffee.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Coffee</h2>
+                    <p>
+                        Black coffee
+                    </p>
+                    <h3>$5.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Minuman/Milk.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Milk</h2>
+                    <p>
+                        Fresh from the Belgian cow
+                    </p>
+                    <h3>$7.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Minuman/Orange_Juice.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Orange Juice</h2>
+                    <p>
+                        Orange juice with fresh orange from the garden
+                    </p>
+                    <h3>$5.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Dessert/Pancake.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Pancake</h2>
+                    <p>
+                        Pancake, request for more additions
+                    </p>
+                    <h3>$10.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+            <div class="menu_card">
+                <div class="menu_image">
+                    <img src="../images/Lauk/Beef Rendang.jpeg">
+                </div>
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <div class="menu_info">
+                    <h2>Beef Rendang</h2>
+                    <p>
+                        Indonesian traditional food from West Sumatra, Padang
+                    </p>
+                    <h3>$20.00</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="login.php" class="menu_btn">Order Now</a>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <?php
+        include 'Footer.php';
+    ?>
+</body>
+</html>
