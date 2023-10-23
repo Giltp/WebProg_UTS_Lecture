0cf660897c7fc949458471a8fc0f9a2e6dc2642c
StrawCraft <strawcraft140904@gmail.com>
2023-10-24 02:42:25 +0700

    new

--- ADMIN/manage_food.php
+++ ADMIN/manage_food.php
@@ -6,44 +6,85 @@
 
             <br />
 
-            <a href="#" class="btn-primary">ADD FOOD</a>
+            <a href="../ADMIN/add-food.php" class="btn-primary">ADD FOOD</a>
 
             <br />
             
             <table class="tbl-full">
                 <tr>
-                    <th>Email</th>
-                    <th>Full Name</th>
-                    <th>Actions</th>
+                    <th>S.N.</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Featured</th>
+                    <th>Active</th>
+                    <th>Action</th>
                 </tr>
 
-                <tr>
-                    <td>tsbpakpahan@gmail.com</td>
-                    <td>Gilbert Pakpahan</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                <?php
+                
+                    $sql = "SELECT * FROM menu";
 
-                <tr>
-                    <td>tsbpakpahan@gmail.com</td>
-                    <td>Gilbert Pakpahan</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                    $res = mysqli_query($conn, $sql);
+                    
+                    $count = mysqli_num_rows($res);
 
-                <tr>
-                    <td>tsbpakpahan@gmail.com</td>
-                    <td>Gilbert Pakpahan</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                    $sn=1;
 
+                    if ($count > 0)
+                    {
+
+                        while ($row = mysqli_fetch_assoc($res))
+                        {
+
+                            $id = $row["id"];
+                            $title = $row['title'];
+                            $price = $row['price'];
+                            $image_name = $row['image_name'];
+                            $featured = $row['featured'];
+                            $active = $row['active'];
+                            ?>
+                            
+                            <tr>
+                                <td><?php echo $sn++; ?> </td>
+                                <td><?php echo $title; ?></td>
+                                <td><?php echo $price; ?></td>
+                                <td>
+                                    <?php
+                                    if($image_name=="")
+                                    {
+                                        echo "<div class='error'>Image not Added.</di>";
+                                    } 
+                                    else
+                                    {
+                                        ?>
+                                            <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name; ?>">
+                                        <?php
+                                    }
+                                    ?>
+                                </td>
+                                <td><?php echo $featured; ?></td>
+                                <td><?php echo $active; ?></td>
+                                <td>
+                                    <a href="#" class="btn-secondary">Update Admin</a>
+                                    <a href="#" class="btn-danger">Delete Admin</a>
+                                </td>
+                            </tr>
+
+                            <?php
+
+
+                        }
+
+                    }
+                    else
+                    {
+                        echo "<tr><td colspan='7' class='error'> Food not Added Yet</td></tr>";
+                    }
+
+                ?>
+
+                
                 
             </table>
 
