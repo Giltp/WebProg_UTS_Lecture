d7e12c8d37e44ee9cb1ac89b0a6bc3f9d9533cf3
Giltp <tsbpakpahan@gmail.com>
2023-10-24 21:42:55 +0700

    Order Admin gw hapus

--- ADMIN/admin_header.php
+++ ADMIN/admin_header.php
@@ -23,7 +23,6 @@
                 <li><a href="manage_admin.php">Admin</a></li>
                 <li><a href="manage_category.php">Category</a></li>
                 <li><a href="manage_food.php">Food</a></li>
-                <li><a href="manage_order.php">Order</a></li>
                 <li><a href="logout_admin.php">Logout</a></li>
             </ul>
         </nav>
